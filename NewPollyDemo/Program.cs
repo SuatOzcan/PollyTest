7fc2f95b60f5e40e5139023bffe1097631f65ef1
suatozcan <email@example.com>
2020-09-08 10:55:04 +0300

    Add project.

---
+++ NewPollyDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using Polly;
+
+namespace NewPollyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < 24; i++)
+            {
+                Console.WriteLine($"Polly Demo Attempt {i}");
+                Console.WriteLine("-------------");
+                Polly.ExecuteRemoteLookUpWithPolly();
+                Console.WriteLine("-------------");
+            }
+        }  
+    }
+}
+
+
